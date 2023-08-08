b8d39ebc1e99f24f3d8332f6986b1c1966bdd532
TimiMaliki <malikitimi@gmail.com>
2023-08-08 10:35:32 +0100

    ----

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,14 +2,13 @@ import Header from "./Header";
 import Nav from "./Nav";
 import About from "./About";
 import Footer from "./Footer";
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 
 const MainLayout = ({ search, setSearch }) => {
     return (
         <div className="App">
-            <Header title="React JS Blog" />
+            <Header title="Blog" />
             <Nav search={search} setSearch={setSearch} />
             <Outlet />
             <Footer />
